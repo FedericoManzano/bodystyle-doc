0eb64436d64d3be31eaab04454478a2018ab7577
FedericoManzano <federicomanzano111@gmail.com>
2020-07-24 10:34:10 -0300

    Documentacion linkeada

--- src/sidebar.js
+++ src/sidebar.js
@@ -10,13 +10,13 @@ import $ from "jquery";
         if($(lista).hasClass("invisible")){
             $(lista).slideDown(200)
             $(lista).removeClass("invisible")
-            $(e.target).children("i").removeClass("fa-chevron-right")
-            $(e.target).children("i").addClass("fa-chevron-down")
+            $(e.target).children("i").removeClass("bs-arrow-up")
+            $(e.target).children("i").addClass("bs-arrow-down")
         }else {
             $(lista).slideUp(200)
             $(lista).addClass("invisible")
-            $(e.target).children("i").addClass("fa-chevron-right")
-            $(e.target).children("i").removeClass("fa-chevron-down")
+            $(e.target).children("i").addClass("bs-arrow-up")
+            $(e.target).children("i").removeClass("bs-arrow-down")
         }
         })
     }
